9346cbe778cbbac8bda3f19199b3a3719d415d00
Tomek Zalewski <11194646+tomzal97@users.noreply.github.com>
2019-10-25 19:20:49 +0200

    Add restaurant class

---
+++ PizzatoWebApi/PizzatoWebApi/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PizzatoWebApi.Models
+{
+    public class Restaurant
+    {
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Adres { get; set; }
+
+        public List<string> Tags { get; set; }
+
+        [JsonIgnore]
+        public long FeatureVector { get; set; }
+    }
+}
