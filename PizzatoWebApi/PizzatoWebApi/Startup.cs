3d1a0da52850523ec90c583a0e54dae447ab213a
Tomek Zalewski <11194646+tomzal97@users.noreply.github.com>
2019-10-25 23:41:02 +0200

    Create reccomend controller, change reccomender to a service

--- PizzatoWebApi/PizzatoWebApi/Startup.cs
+++ PizzatoWebApi/PizzatoWebApi/Startup.cs
@@ -28,6 +28,7 @@ namespace PizzatoWebApi
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSingleton<IRestaurantRepository, RestaurantRepository>();
+            services.AddSingleton<IRecommenderService, RecommenderService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
